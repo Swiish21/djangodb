9e85c5b2b2eae448230ec8d1bd150628504f6df8
Swiiish21 <malcolmotieno@gmail.com>
2024-10-09 10:52:09 +0300

    initial commit

---
+++ djangodb/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Members(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    passwd = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
